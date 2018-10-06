fdaa98a962cad45280887f44ad6ea0c1fea44965
ford42 <ford@ringzero.de>
2018-10-06 14:10:07 +0200

    scrotfbupload.sh fixed for i3-Hotkey and use now main instead of scrot

--- scrotfbupload.sh
+++ scrotfbupload.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-readonly IMGPATH="/home/ford/Daten/Screenshots/"
-readonly IMGNAME=`date +"%Y:%m:%d_%H:%M:%S:%N".png`
-readonly FULLPATH=$IMGPATH$IMGNAME
-scrot "$@" "$FULLPATH"
-readonly OUTPUT="$(fb "$FULLPATH")"
-notify-send "Screenshot uploaded" "$OUTPUT"
+readonly img_path="${HOME}/Daten/Screenshots"
+readonly img_name="$(date +"%Y-%m-%d_%H-%M-%S-%N").png"
+readonly full_path="${img_path}/${img_name}"
 
+case $1 in
+	"-s") maim -s --hidecursor "${full_path}"
+		;;
+	*) maim --hidecursor "${full_path}"
+		;;
+esac
+
+
+readonly output="$(fb "${full_path}")"
+notify-send "Screenshot uploaded" "${output}"
